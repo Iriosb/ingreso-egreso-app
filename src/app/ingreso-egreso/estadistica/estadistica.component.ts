bb87186bdb2f59f5cd58e78f169212d52e5a4a13
Isis Rios <isisrios47e@gmail.com>
2019-04-23 15:43:34 -0500

    seccion 10 lista -app prod

--- src/app/ingreso-egreso/estadistica/estadistica.component.ts
+++ src/app/ingreso-egreso/estadistica/estadistica.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { AppState } from 'src/app/app.reducer';
+//import { AppState } from 'src/app/app.reducer';
 import { Store } from '@ngrx/store';
 import { Subscription } from 'rxjs';
 import { IngresoEgreso } from '../ingreso-egreso.model';
 import { ChartType } from 'chart.js';
 import { MultiDataSet, Label } from 'ng2-charts';
+import * as fromIngresoEgreso from '../ingreso-egreso.reducer';
 
 @Component({
   selector: 'app-estadistica',
@@ -26,11 +27,13 @@ export class EstadisticaComponent implements OnInit {
   public doughnutChartData: number[] = [];
   public doughnutChartType: ChartType = 'doughnut';
 
-  constructor(private store: Store<AppState>) { }
+  constructor(private store: Store<fromIngresoEgreso.AppState>) { }
 
   ngOnInit() {
-    this.subscription = this.store.select('ingresoEgreso')
+
+    this.subscription = this.store.select('ingreso-egreso')
         .subscribe(ingresoEgreso => {
+          console.log(ingresoEgreso)
           this.contarIngresoEgreso(ingresoEgreso.items);
         });
   }
--- src/app/ingreso-egreso/ingreso-egreso.component.ts
+++ src/app/ingreso-egreso/ingreso-egreso.component.ts
@@ -7,6 +7,7 @@ import { Store } from '@ngrx/store';
 import { AppState } from '../app.reducer';
 import { Subscription } from 'rxjs';
 import { ActivarLoadingAction, DesactivarLoadingAction } from '../shared/ui.actions';
+import * as fromIngresoEgreso from '../ingreso-egreso/ingreso-egreso.reducer';
 
 @Component({
   selector: 'app-ingreso-egreso',
@@ -22,7 +23,8 @@ export class IngresoEgresoComponent implements OnInit, OnDestroy {
   cargando: boolean;
 
 
-  constructor(public ingresoEgresoService: IngresoEgresoService, private store: Store<AppState>) { }
+  constructor(public ingresoEgresoService: IngresoEgresoService, 
+              private store: Store<fromIngresoEgreso.AppState>) { }
 
   ngOnInit() {
 
